715e36201ba1c24165c3897c387d78b713783b2c
JJ Kim <jj.dev.001@gmail.com>
2023-01-25 14:01:32 +0900

    build(prettier): set trailingComma to "all"

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,6 @@ prepare().then(() => {
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 });
